48baf3526da0275f69f6858c63e5c8a852ff55e1
ankush0777 <130659966+ankush0777@users.noreply.github.com>
2023-07-21 11:40:05 +0530

    final code

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const connectToMongo=require('./db');
+const express = require('express')
+const path=require('path');
+var cors=require('cors')
+connectToMongo();
+const app = express()
+app.use(express.static(path.join(__dirname,'./build')))
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname,"./build/index.html"));
+});
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+app.listen(port, () => {
+  console.log(`iNotebook backend app listening on port ${port}`)
+})
